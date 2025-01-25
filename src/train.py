2bf732c64841609ff48f3bbd1e2fd0bfb3982399
Nishant Rout <nishant.rout@maximl.com>
2025-01-25 18:07:50 +0530

    feat: hyper param tuning

--- src/train.py
+++ src/train.py
@@ -1,20 +1,33 @@
 import pickle
+import optuna
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
-
 
 def train_model():
-    # Load dataset
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    # Load data
+    data = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2)
 
-    # Train model
-    model = DecisionTreeClassifier()
-    model.fit(X, y)
+    # Objective function for Optuna
+    def objective(trial):
+        n_estimators = trial.suggest_int('n_estimators', 10, 200)
+        max_depth = trial.suggest_int('max_depth', 2, 32, log=True)
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+        return accuracy_score(y_test, model.predict(X_test))
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=50)
+
+    print("Best Parameters:", study.best_params)
 
     # Save model
+    best_model = RandomForestClassifier(**study.best_params)
+    best_model.fit(X_train, y_train)
     with open('src/models/iris_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
+        pickle.dump(best_model, f)
 
 
 if __name__ == '__main__':
